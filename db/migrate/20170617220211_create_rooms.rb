f05854d09355221038be6000350a3aef26ee820d
Nathan Schulzke <nschulzke@gmail.com>
2017-06-17 22:14:04 +0000

    Fix migrate for Room body to text instead of string

--- db/migrate/20170617220211_create_rooms.rb
+++ db/migrate/20170617220211_create_rooms.rb
@@ -2,7 +2,7 @@ class CreateRooms < ActiveRecord::Migration[5.1]
   def change
     create_table :rooms do |t|
       t.string :title
-      t.string :body
+      t.text :body
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617220211) do
+ActiveRecord::Schema.define(version: 20170617220503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "paths", force: :cascade do |t|
+    t.string "choice"
+    t.integer "from_room_id"
+    t.integer "to_room_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "rooms", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
