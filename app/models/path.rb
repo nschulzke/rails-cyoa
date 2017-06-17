f8e180076766c5976d76f3d6b350389c6befe049
Nathan Schulzke <nschulzke@gmail.com>
2017-06-17 23:47:56 +0000

    Change order for room and path

--- app/models/path.rb
+++ app/models/path.rb
@@ -1,4 +1,5 @@
 class Path < ApplicationRecord
   belongs_to :from_room, class_name: "Room"
   belongs_to :to_room, class_name: "Room"
+  default_scope { order(created_at: :asc) }
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,5 @@
 class Room < ApplicationRecord
   has_many :paths_in, class_name: "Path", foreign_key: "to_room_id"
   has_many :paths_out, class_name: "Path", foreign_key: "from_room_id"
+  default_scope { order(created_at: :asc) }
 end
