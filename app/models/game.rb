94180a9a7562a98929457c6aa197aa00bf508d66
Nathan Schulzke <nschulzke@gmail.com>
2017-07-19 19:50:51 -0600

    Make a room belong to a game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ApplicationRecord
   belongs_to :first_room, class_name: 'Room'
   belongs_to :user
+  has_many :rooms
 
   accepts_nested_attributes_for :first_room
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,6 @@
 class Room < ApplicationRecord
   has_many :paths_in, class_name: "Path", foreign_key: "to_room_id"
   has_many :paths_out, class_name: "Path", foreign_key: "from_room_id"
+  belongs_to :game
   default_scope { order(created_at: :asc) }
 end
