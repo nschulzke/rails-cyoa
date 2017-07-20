f78a12f2eaa6131e73ab3bf205c62887eff5f312
Nathan Schulzke <nschulzke@gmail.com>
2017-07-20 16:41:13 -0600

    Add login/logout links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def login_helper css_class = ''
+    if current_user
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: css_class)
+    else
+      (link_to "Register", new_user_registration_path, class: css_class) +
+      " " +
+      (link_to "Login", new_user_session_path, class: css_class)
+    end
+  end
 end
