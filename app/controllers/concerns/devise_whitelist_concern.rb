8fab341957a2ee76f95b337114baaf0f98244ab1
Nathan Schulzke <nschulzke@gmail.com>
2017-07-19 18:03:07 -0600

    Add devise user model

---
+++ app/controllers/concerns/devise_whitelist_concern.rb
@@ -0,0 +1,11 @@
+module DeviseWhitelistConcern
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_presence_of :username
+
+  validates :username, format: { with:    /\A[a-z0-9A-Z]*\z/,
+                                 message: "must contain only letters and numbers" }
 end
